825d4ce183c004905ebc8824c7c5766ad4c5f5d2
逄绛 <pj@pjs-mb-pro.gsm1900.org>
2017-09-01 11:22:10 -0700

    add first test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FirstTestComponent } from './first-test/first-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstTestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/address.ts
@@ -0,0 +1,6 @@
+export class Address {
+    address: string;
+    city: string;
+    provice: string;
+    zip: number;
+}
